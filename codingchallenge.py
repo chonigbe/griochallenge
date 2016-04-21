2d84df70b32ffde7a8d3bd1e7fc96ce3da169c6c
Celia Honigberg <chonigbe@wellesley.edu>
2016-04-20 22:49:29 -0400

    Adding relevent code to repository

---
+++ codingchallenge.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+
+##################################################
+# Author: Celia Honigberg
+# Date: 04/20/16
+# Description: Grio Code Challenge
+##################################################
+
+# standard library imports
+# related third party imports
+# local application/library specific imports
+import re
+import sys
+import time
+import httplib
+
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
+
+def regular_word(word,charregex,punctation):
+  """Takes in a string
+  and a regular expression of what characters matter
+  and a tuple of ending punctuation marks
+  returns a string that is reversed when string has an odd
+  number of characters.
+  """
+  length = len(re.findall(charregex, word))
+  if length % 2 != 0:
+    if word.endswith(punctation):
+      word = (word[length-1::-1] 
+      	      + word[length:])
+    else:
+      word = word[::-1] 
+  return word
+
+def is_url(url):
+  """Takes in a string
+  returns true if the string is a properly formatted url
+  MORE WORK NEEDED HERE. 
+  """
+  return re.match('(^http|ftp|https)://', url)
+
+def is_lat_long(latlong):
+  """Takes in a string
+  returns true if the string has properly formatted 
+  longitude and latitude coordinates
+  """
+  return re.match('(?:http|ftp|https)://', latlong)
+
+def shorten_url(url):
+  """Takes in a url 
+  returns the url without https:// or http://
+  """
+  return 'grio.com'
+
+def get_address(latlong):
+  """Takes in longitude and latitude coordinates 
+  returns the street address from google maps api. 
+  """
+  return 'FML T_____T OTL'
+
+def translate_content(line):
+  """Takes in a file
+  and prints out each line in the file with some alterrations. 
+  """
+  current = ''
+  for word in line.split():
+    if (is_url(word)):
+      word = shorten_url(word)
+    elif (is_lat_long(word)):
+      word = word + ' ' + get_address(word)
+    else: 
+      word = regular_word(word
+    	                    , '[a-zA-Z\-]'
+    	                    , tuple(['.',',',':','?','!']))
+    # if (at end of line):
+    #   current += word
+    # else: 
+    current += word + ' '
+  return current
+
+def print_content(file):
+  """Takes in a file
+  and prints each processed line in the given file.
+  """
+  with open(file) as f:
+    # processes and prints line by line
+    for line in f:
+      print translate_content(line)
+
+
+# For(all the lines in the file) {
+# 	read one line of input file
+# 	for (all the words in the line) {
+# 		take each word, check if it is a 
+# 		1. url (verfication, ignore lat longs in a url regex: http:// or https:// + domain (white list characters?) + . + small number of address characters + and infinite number of /text/text/text) (before you start writing you have to figure out what this is going to involve)
+# 		2. lat long (do verification to make sure) regex that handles [+lat+,+long+] and then verify that it's actually a place and then print out its place then print out it. 
+# 		3. Check and see if it has an odd number of characters, if it does, reverse it. 
+# 		4. If it has an even number of characters, just print it out. 
+# 	}
+# }
+
+def main():
+  start = time.clock()
+  print_content('giventest.txt')
+  # translate_content('If you want to visit Grio, you can go to \
+  #       http://grio.com or visit us at [37.78667, -122.39782]. Thank you.')
+  end = time.clock()
+  print end - start
+  # start = time.clock()
+  # translate_content('testlargestring.txt')
+  # end = time.clock()
+  # print end - start
+  # display some lines
+
+if __name__ == "__main__": main()
---
+++ teststringmethods.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import unittest
+
+import codingchallenge as cc
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_regular_word(self):
+    # Test even word
+    self.assertEqual(cc.regular_word('word', '[a-zA-Z\-]'
+                                 , tuple(['.',',',':','?']))
+                                 , 'word')
+    # Test odd word
+    self.assertEqual(cc.regular_word('words', '[a-zA-Z\-]'
+                                 , tuple(['.',',',':','?']))
+                                 , 'sdrow')
+    # Test odd word with punctionation
+    self.assertEqual(cc.regular_word('words!', '[a-zA-Z\-]'
+                                 , tuple(['.',',',':','?','!']))
+                                 , 'sdrow!')
+
+  def test_is_url(self):
+    # general domain http
+    self.assertTrue(cc.is_url('http://grio.com'),1)
+    # general domain https
+    self.assertTrue(cc.is_url('https://grio.com'),1)
+    # domain + path 
+    self.assertTrue(cc.is_url('http://hammerfishslap.tumblr.com'),1)
+    # has a path after the .com
+    self.assertTrue(cc.is_url('http://grio.com/portfolio'),1)
+    # not well formed, no dot anything
+    #self.assertFalse(cc.is_url('http://grio/portfolio'),1)
+    # not a real url 
+    #self.assertFalse(cc.is_url('http://portfoliogrio.com/'),1)
+
+
+  def test_translate_content(self):
+    self.assertTrue(cc.translate_content('If you want to visit Grio, you can go to \
+      http://grio.com or visit us at [37.78667, -122.39782]. Thank you.'),
+      'If uoy want to tisiv Grio, uoy nac go to moc.oirg//:ptth or tisiv \
+      us at [37.78667, -122.39782]. knahT uoy. ')
+    self.assertFalse('Foo'.isupper())
+
+  def test_split(self):
+    s = 'hello world'
+    self.assertEqual(s.split(), ['hello', 'world'])
+    # check that s.split fails when the separator is not a string
+    with self.assertRaises(TypeError):
+        s.split(2)
+
+if __name__ == '__main__':
+  unittest.main()
